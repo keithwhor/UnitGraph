b083d75189758453c5c16255da86621d7756fb40
Keith Horwood <keithwhor@gmail.com>
2015-07-16 16:45:45 -0700

    more tests

--- test/runner.js
+++ test/runner.js
@@ -5,7 +5,6 @@ let expect = require('chai').expect;
 describe('Test Suite', function() {
 
   const UG = require('../module.js');
-  let graph = new UG.Graph();
 
   const Unit = require('../lib/unit/unit.js');
   const Node = require('../lib/unit/node.js');
@@ -18,6 +17,8 @@ describe('Test Suite', function() {
   describe('UnitGraph', function() {
 
     describe('Graph', function() {
+
+      let graph = new UG.Graph();
 
       it('Should have the appropriate hidden properties', function() {
 
@@ -77,41 +78,90 @@ describe('Test Suite', function() {
 
       });
 
-      describe('Collections', function() {
+    });
 
-        let nc = graph.nodes('testNode');
-        let ec = graph.edges('testEdge');
+    describe('Collections', function() {
 
-        describe('NodeCollection', function() {
+      let graph = new UG.Graph();
+      graph.createNode('testNode', {id: 1, hello: 'world'});
+      graph.createEdge('testEdge', {id: 1, hello: 'world'});
 
-          it('Should have the correct name', function() {
+      let nc = graph.nodes('testNode');
+      let ec = graph.edges('testEdge');
 
-            expect(nc.name()).to.equal('testNode');
+      describe('NodeCollection', function() {
 
-          });
+        it('Should have the correct name', function() {
 
-          it('Should not have any indices to begin', function() {
+          expect(nc.name()).to.equal('testNode');
 
-            expect(nc.indices().length).to.equal(0);
+        });
 
-          });
+        it('Should not have any indices to begin', function() {
 
-          it('Should be able to create an index', function() {
+          expect(nc.indices().length).to.equal(0);
 
-            nc.createIndex('id');
-            ec.createIndex('id');
+        });
 
-            expect(nc.indices().length).to.equal(1);
-            expect(nc.indices()[0]).to.equal('id');
-            expect(nc._indices).to.contain.all.keys(['id']);
+        it('Should be able to create an index', function() {
 
-          });
+          nc.createIndex('id');
+          ec.createIndex('id');
 
-          it('Should be able to find node by id', function() {
+          expect(nc.indices().length).to.equal(1);
+          expect(nc.indices()[0]).to.equal('id');
+          expect(nc._indices).to.contain.all.keys(['id']);
 
-            expect(nc.find(1)).to.equal(graph._nodes[0]);
+        });
 
-          });
+        it('Should be able to find node by id', function() {
+
+          expect(nc.find(1)).to.equal(graph._nodes[0]);
+
+        });
+
+      });
+
+    });
+
+    describe('Units', function() {
+
+      let graph = new UG.Graph();
+      graph.createNode('testNode', {id: 1, hello: 'world'});
+      graph.createEdge('testEdge', {id: 1, hello: 'world'});
+
+      let n = graph._nodes[0];
+      let e = graph._edges[0];
+
+      describe('Node', function() {
+
+        it('should be able to set a property', function() {
+
+          expect(n.set('a', 'b')).to.equal('b');
+
+        });
+
+        it('should be able to get a property', function() {
+
+          expect(n.get('a')).to.equal('b');
+
+        });
+
+        it('should have the property', function() {
+
+          expect(n.has('a')).to.equal(true);
+
+        });
+
+        it('should unset the property', function() {
+
+          expect(n.unset('a')).to.equal(true);
+
+        });
+
+        it('should return undefined for get of unset property', function() {
+
+          expect(n.get('a')).to.be.undefined;
 
         });
 
